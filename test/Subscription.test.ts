9d11f70f53869088b2a4afa379ad1b70cb807698
benbaessler <mail@benbaessler.com>
2022-08-04 00:47:28 +0100

    Added tests

--- test/Subscription.test.ts
+++ test/Subscription.test.ts
@@ -1,10 +1,9 @@
 import { expect } from "chai";
-import { describe } from 'mocha'
 import { ethers } from "hardhat";
-import { SubscriptionFactory } from '../typechain-types/contracts/SubscriptionFactory';
-import { Token } from '../typechain-types/contracts/PaymentToken.sol/Token';
+import { SubscriptionFactory } from "../typechain-types";
+import { Token } from '../typechain-types';
 
-describe('eth-subscription', () => {
+describe('Subscription Contract', () => {
 
   let alice: any
   let bob: any
@@ -20,24 +19,25 @@ describe('eth-subscription', () => {
     
     const SubscriptionFactory = await ethers.getContractFactory('SubscriptionFactory')
     source = await SubscriptionFactory.deploy(token.address)
+
+    await token.approve(source.address, 10000)
   })
 
-  describe('subscribe()', () => {
-    it('should add a new subscription', async () => {
-      await token.approve(source.address, 10)
+  describe('subscribe', () => {
+    it('adds a new subscription', async () => {
       await source.subscribe(1)
   
       const contractBalance = await token.balanceOf(source.address)
       const userBalance = await token.balanceOf(alice.address)
-      const isSubscriber = await source.isSubscriber(alice.address)
+      const isSubscribed = await source.isSubscribed(alice.address)
+      const subscriberCount = await source.subscriberCount()
   
       expect(contractBalance).to.equal(10)
       expect(userBalance).to.equal(9990)
-      expect(isSubscriber).to.be.true
+      expect(isSubscribed).to.be.true
     })
   
-    it('should add a subscription with forward payment', async () => {
-      await token.approve(source.address, 100)
+    it('adds a subscription with forward payment', async () => {
       await source.subscribe(10)
   
       const totalReserve = await source.totalUserReserve()
@@ -47,27 +47,148 @@ describe('eth-subscription', () => {
       expect(userReserve).to.equal(90)
     })
   
-    it('should revert if the caller has insufficient funds', async () => {
-      await token.transfer(alice.address, 5)
-      await token.connect(alice).approve(source.address, 5)
-      await expect(source.connect(alice).subscribe(1)).to.be.revertedWithCustomError
+    it('reverts if the caller has insufficient funds', async () => {
+      await token.transfer(bob.address, 5)
+      await token.connect(bob).approve(source.address, 10)
+      await expect(source.connect(bob).subscribe(1)).to.be.revertedWithCustomError(source, 'InsufficientFunds')
     })
 
-    it('should revert if the caller is already subscribed', async () => {
-      await token.approve(source.address, 20)
+    it('reverts if the caller is already subscribed', async () => {
       await source.subscribe(1)
-      await expect(source.subscribe(1)).to.be.revertedWithCustomError
+      await expect(source.subscribe(1)).to.be.revertedWithCustomError(source, 'AlreadySubscribed')
     })
   })
 
-  describe('unsubscribe()', () => {
-    it('should remove subscription from caller', async () => {
-      await token.approve(source.address, 10)
+  describe('unsubscribe', () => {
+    it('removes subscription from caller', async () => {
       await source.subscribe(1)
 
       await source.unsubscribe()
-      const isSubsc
+      expect(await source.isSubscribed(alice.address)).to.equal(false)
+    })
+
+    it('withdraws all reserve tokens to caller', async () => {
+      await source.subscribe(10)
+
+      await source.unsubscribe()
+      const totalReserve = await source.totalUserReserve()
+      const userReserve = await source.reserveAmount(alice.address)
+
+      expect(totalReserve).to.equal(0)
+      expect(userReserve).to.equal(0)
+      expect(await token.balanceOf(source.address)).to.equal(10)
+      expect(await token.balanceOf(alice.address)).to.equal(9990)
+    })
+
+    it('reverts if the caller is not subscribed', async () => {
+      await expect(source.unsubscribe()).to.be.revertedWithCustomError(source, 'NoSubscription') 
     })
   })  
 
+  describe('deposit', async () => {
+
+    it('deposits funds into user reserve', async () => {
+      await source.subscribe(1)
+
+      await source.deposit(9)
+
+      const totalReserve = await source.totalUserReserve()
+      const userReserve = await source.reserveAmount(alice.address)
+  
+      expect(totalReserve).to.equal(90)
+      expect(userReserve).to.equal(90)
+    })
+
+    it('reverts if the caller is not subscribed', async () => {
+      await expect(source.deposit(1)).to.be.revertedWithCustomError(source, 'NoSubscription') 
+    })
+
+    it('reverts if the caller has insufficient funds', async () => {
+      await token.transfer(bob.address, 15)
+      await token.connect(bob).approve(source.address, 15)
+
+      await source.connect(bob).subscribe(1)
+
+      await expect(source.connect(bob).deposit(1)).to.be.revertedWithCustomError(source, 'InsufficientFunds')
+    })
+  })
+
+  describe('withdraw', () => {
+    it('withdraws reserve funds to caller', async () => {
+      await source.subscribe(10)
+      await source.withdraw(9)
+
+      const totalReserve = await source.totalUserReserve()
+      const userReserve = await source.reserveAmount(alice.address)
+
+      expect(totalReserve).to.equal(0)
+      expect(userReserve).to.equal(0)
+      expect(await token.balanceOf(source.address)).to.equal(10)
+      expect(await token.balanceOf(alice.address)).to.equal(9990)
+    })
+
+    it('reverts if user reserve is empty', async () => {
+      await source.subscribe(1)
+      await expect(source.withdraw(1)).to.be.revertedWithCustomError(source, 'EmptyReserve')
+    })
+
+    it('reverts if withdraw amount exceeds user reserve', async () => {
+      await source.subscribe(10)
+      await expect(source.withdraw(10)).to.be.revertedWithCustomError(source, 'InsufficientReserve')
+    })
+
+    it('reverts if caller is not subscribed', async () => {
+      await expect(source.withdraw(1)).to.be.revertedWithCustomError(source, 'NoSubscription')
+    })
+  })
+
+  describe('withdrawFees', () => {
+    it('withdraws accumulated fees to owner', async () => {
+      await source.subscribe(1)
+      await source.withdrawFees()
+
+      expect(await token.balanceOf(source.address)).to.equal(0)
+      expect(await token.balanceOf(alice.address)).to.equal(10000)
+    })
+
+    it('reverts if no fees have been accumulated', async () => {
+      await expect(source.withdrawFees()).to.be.revertedWithCustomError(source, 'EmptyReserve')
+    })
+
+    it('reverts if caller is not owner', async () => {
+      await source.subscribe(1)
+      await expect(source.connect(bob).withdrawFees()).to.be.revertedWith('Ownable: caller is not the owner')
+    })
+  })
+
+  describe('updateSubscription', () => {
+    it('updates subscription status', async () => {
+      await source.subscribe(2)
+      await ethers.provider.send('evm_increaseTime', [30 * 24 * 60 * 60])
+      await source.updateSubscription()
+
+      const subscription = await source.getSubscription()
+      expect(subscription.period).to.equal(2)
+      expect(subscription.active).to.be.true
+    })
+
+    it('deactivates subscription if user has insuffient reserve', async () => {
+      await source.subscribe(1)
+      await ethers.provider.send('evm_increaseTime', [30 * 24 * 60 * 60])
+      await source.updateSubscription()
+
+      const subscription = await source.getSubscription()
+      expect(subscription.active).to.be.false
+    })
+
+    it('takes periodical fee from user reserve', async () => {
+      await source.subscribe(2)
+
+      await ethers.provider.send('evm_increaseTime', [31 * 24 * 60 * 60])
+      await source.updateSubscription()
+
+      expect(await source.reserveAmount(alice.address)).to.equal(0)
+    })
+
+  })
 })
